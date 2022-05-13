f709025227a434cdad948e5768c61b4548767b24
Willibrordus Bayu <willibayu28@gmail.com>
2022-05-13 19:32:40 +0700

    First Commit

---
+++ collections/query/map_reduce_category_sold.js
@@ -0,0 +1,19 @@
+/* create function to map category and bottles sold  */
+var mapCategorySold = function() {
+	emit(this.category, this.bottles_sold);
+};
+
+/* create function to reduce category and bottles sold */
+var reduceCategorySold = function(keyCategoryName, valuesBottlesSold) {
+	return Array.sum(valuesBottlesSold);
+};
+
+/* create MapReduce */
+db.iowa_liquor_sales.mapReduce (
+	mapCategorySold,
+	reduceCategorySold,
+	{ out: "map_reduce_category_sold" }
+)
+
+/* check result */
+db.map_reduce_category_sold.find().pretty()
