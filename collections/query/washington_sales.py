a40307011b6105d3d8a9be20a464d01de31a4621
Bayu Nova <willibayu28@gmail.com>
2022-05-13 14:44:47 +0700

    First Commit

---
+++ collections/query/washington_sales.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb://localhost:27017/')
+filter={
+    'city': 'Washington'
+}
+project={
+    '_id': 0, 
+    'date': 0
+}
+
+result = client['db-test']['iowa_liquor_sales'].find(
+  filter=filter,
+  projection=project
+)
